b358a87c8594d013e4086c27982dc526adf66ab2
Isha Shetty <isha.godyal29292@gmail.com>
2022-05-10 08:19:57 +0530

    SC2.Capture WebElement Screenshot using Selenium

--- src/programs/NewWindow.java
+++ src/programs/NewWindow.java
@@ -1,20 +1,29 @@
 package programs;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.Set;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class NewWindow {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		//Invoking Multiple Windows/Tabs from Selenium using one driver Instance
-		//Scenario: Navigate to https://rahulshettyacademy.com/angularpractice/
+		
+		//Scenario 1: Navigate to https://rahulshettyacademy.com/angularpractice/
 		//Fill the "Name" field with the first course name available at "https://rahulshettyacademy.com/"
+		
+		
+		
 		//a[href*='https://courses.rahulshettyacademy.com/p']
 		String projectPath = System.getProperty("user.dir");
 		
@@ -33,8 +42,15 @@ public class NewWindow {
 		driver.get("https://rahulshettyacademy.com/");
 		String courseName = driver.findElements(By.cssSelector("a[href*='https://courses.rahulshettyacademy.com/p']")).get(1).getText();
 		driver.switchTo().window(parentWindowId);
-		driver.findElement(By.cssSelector("[name='name']")).sendKeys(courseName);
-		driver.quit();
+		WebElement name = driver.findElement(By.cssSelector("[name='name']"));
+		name.sendKeys(courseName);
+		
+		//Scenario 2: Capture WebElement Screenshot using Selenium
+		//Get Height Width of Web Element using Selenium
+		File file = name.getScreenshotAs(OutputType.FILE);
+		
+		FileUtils.copyFile(file, new File("logo.png"));
+		//driver.quit();
 		
 		
 		
